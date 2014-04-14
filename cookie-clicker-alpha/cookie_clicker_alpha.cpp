74a169f374910e96c24610751fed909ea4ee9f63
Ahmed Siouani <ahmed.siouani@gmail.com>
2014-04-14 10:10:09 +0200

    [Cookie-Clicker-alpha] Fix a bad commit - Upload the right program

--- cookie-clicker-alpha/cookie_clicker_alpha.cpp
+++ cookie-clicker-alpha/cookie_clicker_alpha.cpp
@@ -1,5 +1,5 @@
 //
-//  magic-trick.cpp
+//  cookie_clicker_alpha.cpp
 //  google_code_jam
 //
 //  Created by ahmed on 4/12/14.
@@ -39,80 +39,42 @@ string readFile(ifstream& inputFile)
     return newLine;
 }
 
-int fillGrid(ifstream& inputFile, int grid[4][4])
-{
-    string line;
-    
-    for (int a = 0; a < 4; a++) {
-        
-        line = readFile(inputFile);
-        auto v = explode(line, ' ');
-        
-        for (int b = 0; b < 4; b++) {
-            string position =  v.at(b);
-            grid[a][b] = atoi(position.c_str());
-        }
-    }
-    
-    return 0;
-}
-
 int main(int argc, const char * argv[])
 {
     ifstream inputFile ("input.txt");
     ofstream outputFile ("output.txt");
     
     string line;
-    
     int numberOfCases;
-    int choices[2], grid1[4][4], grid2[4][4];
     
     line = readFile(inputFile);
     numberOfCases = atoi(line.c_str());
     
     for (int i = 0; i < numberOfCases; i++) {
-        line = readFile(inputFile);
-        choices[0] = atoi(line.c_str());
+       line = readFile(inputFile);
+        auto v = explode(line, ' ');
+
+        float c = stof(v.at(0).c_str());
+        float f = stof(v.at(1).c_str());
+        float x = stof(v.at(2).c_str());
+
+        int perSecond = 2;
+        double time = 0;
+        bool checkIfINeedAFarm = true;
         
-        fillGrid(inputFile, grid1);
-        
-        line = readFile(inputFile);
-        choices[1] = atoi(line.c_str());
-        
-        fillGrid(inputFile, grid2);
-        
-        int firstMatch[4], secondMatch[4];
-        for (int k = 0; k < 4; k++) {
-            firstMatch[k]  = grid1[choices[0] - 1][k];
-            secondMatch[k] = grid2[choices[1] - 1][k];
-        }
-        
-        string message;
-        int selectedCard;
-        bool alreadyMatch = false;
-        
-        for (int x = 0; x < 4; x++) {
-            for (int y = 0; y < 4; y++) {
-                if (firstMatch[x] == secondMatch[y]) {
-                    selectedCard = firstMatch[x];
-                    if (alreadyMatch) {
-                        message = "Bad magician!";
-                    }
-                    alreadyMatch = true;
-                }
-                
+        while (checkIfINeedAFarm) {
+            if (x/(perSecond + f) + (c/perSecond) < (x/perSecond)) {
+                time = time + c/perSecond;
+                perSecond = perSecond + f;
+            } else {
+                checkIfINeedAFarm = false;
+                time = time + x/perSecond;
             }
         }
-        
-        if (!alreadyMatch) {
-            message = "Volunteer cheated!";
-        }
-        
+
         if (outputFile.is_open()) {
-            if (!message.empty())
-                outputFile << "Case #" << i+1 << ": " << message << '\n';
-            else
-                outputFile << "Case #" << i+1 << ": " << selectedCard << '\n';
+           outputFile.precision(7);
+            outputFile << "Case #" << i+1 << ": " << fixed << time << '\n';
         }
     }
     inputFile.close();
